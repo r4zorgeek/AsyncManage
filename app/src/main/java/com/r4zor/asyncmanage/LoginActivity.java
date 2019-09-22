3e2473892fd452446e0bffb9606a65885dec29d1
r4zorgeek <abhishekmane747@gmail.com>
2019-09-22 09:55:42 +0530

    user token added in shared pref

--- app/src/main/java/com/r4zor/asyncmanage/LoginActivity.java
+++ app/src/main/java/com/r4zor/asyncmanage/LoginActivity.java
@@ -3,7 +3,9 @@ package com.r4zor.asyncmanage;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -390,8 +392,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 // finish();
+
+                // save auth token in shared pref
+                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("user_token", AUTH_TOKEN);
+                editor.commit();
+
+                // start main activity
                 Intent i = MainActivity.newIntent(LoginActivity.this);
                 startActivity(i);
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
